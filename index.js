687af4651f064082aa1d716d08a50637894431e5
harquail <nook@harquail.com>
2016-02-02 17:07:43 -0500

    switch to heroku

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ console.log("chansey");
 
 // var port = Number();
 app.set('port', process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 5000);
-app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || process.env.IP || "127.0.0.1");
 app.listen(app.get('port'),app.get('ip'), function() {
   console.log("Listening on " + app.get('port'));
 });
