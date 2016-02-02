15d0db6986aac76b02e88b04d7cadaea62e296f3
harquail <nook@harquail.com>
2016-02-02 13:33:58 -0500

    more openshift config

--- index.js
+++ index.js
@@ -79,7 +79,8 @@ console.log("chansey");
 }
 
 
-var port = Number(process.env.OPENSHIFT_NODEJS_PORT || 5000);
+var port = Number(process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 5000);
+app.set('ip', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
